957060beb113cbd3938d4c19951e700dd971f112
HelloWorldCN <tdm_2010@swjtu.edu.cn>
2013-01-07 14:59:56 +0800

    初始创建

---
+++ src/main/java/org/swjtu/helloworldcn/mine/MineMain.java
@@ -0,0 +1,18 @@
+package org.swjtu.helloworldcn.mine;
+
+public class MineMain {
+
+	/**
+	 * 西南交通大学
+	 * @author Tang 
+	 * @since 2012-9
+	 * Maximal Information Coefficient(MIC) measure
+	 * 最大信息系数的hadoop实现的入口程序
+	 * App Main
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
